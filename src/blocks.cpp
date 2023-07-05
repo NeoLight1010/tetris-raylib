9bdbda65d7c17c54d4847df27761caa7f64abc0d
Anthony Suárez <neolight1010@gmail.com>
2023-07-04 21:24:59 -0500

    Fix SBlock rotation

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -82,7 +82,7 @@ public:
                     Position{2, 2}};
     rotations[2] = {Position{1, 1}, Position{1, 2}, Position{2, 0},
                     Position{2, 1}};
-    rotations[3] = {Position{0, 0}, Position{0, 1}, Position{1, 1},
+    rotations[3] = {Position{0, 0}, Position{1, 0}, Position{1, 1},
                     Position{2, 1}};
   }
 
