d5bef23f02171356f93c52cb27cd6ebfc34195b0
Anthony Suárez <neolight1010@gmail.com>
2023-07-11 19:34:53 -0500

    Center score indicator

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,43 @@
 #include "colors.h"
 #include "game.h"
 #include <cstddef>
+#include <iostream>
 #include <raylib.h>
 #include <stdlib.h>
 
-void drawHUD(Font font, bool gameOver, int score) {
+void drawScoreText(int score, float backgroundX, float backgroundWidth,
+                   Font font) {
+  int scoreTextFontSize = 38;
+  float scoreTextSpacing = 2;
+
+  auto scoreString = std::to_string(score);
+
+  int scoreTextWidth = MeasureText(scoreString.c_str(), scoreTextFontSize);
+
+  float sidesPadding = (backgroundWidth - scoreTextWidth) / 2;
+  float scoreTextX = backgroundX + sidesPadding;
+
+  DrawTextEx(font, scoreString.c_str(), {scoreTextX, 65}, scoreTextFontSize,
+             scoreTextSpacing, WHITE);
+}
+
+void drawScoreIndicator(Font font, int score) {
   DrawTextEx(font, "Score", {365, 15}, 38, 2, WHITE);
-  DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, BLUE);
-  DrawTextEx(font, std::to_string(score).c_str(), {330, 65}, 38, 2, WHITE);
+
+  float backgroundX = 320;
+  float backgroundY = 55;
+  float backgroundWidth = 170;
+  float backgroundHeight = 60;
+
+  DrawRectangleRounded(
+      {backgroundX, backgroundY, backgroundWidth, backgroundHeight}, 0.3, 6,
+      BLUE);
+
+  drawScoreText(score, backgroundX, backgroundWidth, font);
+}
+
+void drawHUD(Font font, bool gameOver, int score) {
+  drawScoreIndicator(font, score);
 
   DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
   DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, BLUE);
@@ -34,7 +64,7 @@ bool deltaTimeHasPassed(double interval) {
 int main() {
   auto game = Game();
 
-  Font font = LoadFontEx("fonts/monogram.ttf", 64, NULL, 0);
+  Font font = GetFontDefault();
 
   int windowWidth = game.grid.NUM_COLUMNS * game.CELL_SIZE + 200;
   int windowHeight = game.grid.NUM_ROWS * game.CELL_SIZE + 20;
